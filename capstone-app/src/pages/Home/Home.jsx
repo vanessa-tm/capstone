936e474a18369e84f5ee67b9c3d72ef5eae32087
vanessa-tm <vanessatmiller@gmail.com>
2025-01-16 14:38:13 -0700

    display all lists items to home page

--- capstone-app/src/pages/Home/Home.jsx
+++ capstone-app/src/pages/Home/Home.jsx
@@ -1,7 +1,36 @@
+import axios from "axios";
 import "./Home.scss";
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import iconsEdit from "../../assets/icons/edit-button.png";
+import iconsDelete from "../../assets/icons/delete.png";
 
 function Home () {
+
+    const [lists, setLists] = useState([]);
+
+    useEffect(() => {
+
+        async function getLists() {
+            try {
+
+                const response = await axios.get("http://localhost:8080/lists/");
+                setLists(response.data);
+
+            } catch (error) {
+                console.error("Error getting lists", error);
+            }
+                
+        }
+
+        getLists();
+
+    }, []);
+
+   
+
+
+
     return (
         <>
             <div className="hero">
@@ -13,10 +42,23 @@ function Home () {
                 <Link to="/grocery-lists" >
                     <button className="hero__button">Create New List</button>
                 </Link>
-                
             </div>
+            <div >
+                  {lists.map ((list) => (
+                    <div key={list.id} className="list" >
+                        <div  className="list__item-container">
+                            <h3 className="list__item" >{list.list_name}</h3>
+                        </div>
+                        <div className="list__image-container">
+                            <img src={iconsEdit} className="list__image" />
+                            <img src={iconsDelete} className="list__image" />
+                        </div>
+                    </div>
+                ))} 
+            </div>
+
         </>
-    )
+    );
 }
 
 export default Home;
